cd98a02fa9479d712aa1f90bd4bc53a95713df6c
Tanakorn Rattanajariya <lufas2603@gmail.com>
2021-06-18 19:10:27 +0700

    Update agent

--- redux/services/agent.js
+++ redux/services/agent.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 const API_ROOT = process.env.BACKEND_URI;
 const headers = () => ({});
-const agent = token => {
+const agent = (token) => {
   // let user = localStorage.getItem("use");
   // const tk = JSON.parse(user).token;
   return axios.create({
     baseURL: `${API_ROOT}`,
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json"
-      //Authorization: `Token ${tk}`
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
     },
-    timeout: 20000
+    timeout: 20000,
   });
 };
 
